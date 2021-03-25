18a094421d5ca29e6320a0c7b8eead73e19ec487
Bernd Oeggl <bernd@webman.at>
2021-03-25 16:36:29 +0100

    add stop object

--- nz/main.js
+++ nz/main.js
@@ -1,13 +1,22 @@
 
+let stop = {
+  nr: 22,
+  name: "Tongariro National Park",
+  lat: -39.29,
+  lng: 175.56,
+  user: "webmapping",
+  wikipedia: "https://en.wikipedia.org/wiki/Tongariro_National_Park"
+}
+
 const map = L.map("map", {
-  center: [ -39.29, 175.56  ],
+  center: [ stop.lat, stop.lng ],
   zoom: 13,
   layers: [
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
   ]
 });
 
-let mrk = L.marker([ -39.29, 175.56 ]).addTo(map);
-mrk.bindPopup('Tongariro National Park');
+let mrk = L.marker([ stop.lat, stop.lng ]).addTo(map);
+mrk.bindPopup('Tongariro National Park').openPopup();
 
 console.log(document.querySelector("#map"));
