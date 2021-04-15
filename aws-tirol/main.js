ddb2e6f7134c148605384ee05fd870082854cc27
Bernd Oeggl <bernd.oeggl@uibk.ac.at>
2021-04-15 16:42:39 +0200

    add fetch for geodata from ogd

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -18,3 +18,20 @@ let layerControl = L.control.layers({
         L.tileLayer.provider('BasemapAT.overlay')
     ])
 }).addTo(map);
+
+
+let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
+
+fetch(awsUrl)
+    .then(response => response.json())
+    .then(json => {
+        console.log('Daten konvertiert: ', json);
+        for (station of json.features) {
+            console.log('Station: ', station);
+            let marker = L.marker(
+                [station.geometry.coordinates[1],
+                station.geometry.coordinates[0]]
+                );
+            marker.addTo(map);
+        }
+    });
