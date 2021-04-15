28f5c814d57544da3acea9cdc61c14be898a4621
Bernd Oeggl <bernd.oeggl@uibk.ac.at>
2021-04-15 16:02:50 +0200

    add baselayer plugin and basemapAT layers

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -1,1 +1,15 @@
 
+let basemapGray = L.tileLayer.provider('BasemapAT.grau');
+
+let map = L.map("map", {
+    center: [47, 11],
+    zoom: 9,
+    layers: [
+        basemapGray
+    ]
+});
+
+let layerControl = L.control.layers({
+    "BasemapAT.grau": basemapGray,
+    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto')
+}).addTo(map);
