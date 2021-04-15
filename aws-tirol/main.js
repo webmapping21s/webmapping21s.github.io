eee0dc3d643361bc4545e64b75ef631e3a1e56cd
Bernd Oeggl <bernd.oeggl@uibk.ac.at>
2021-04-15 17:49:13 +0200

    add div icon with values for snow height

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -23,7 +23,11 @@ let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
 
 let awsLayer = L.featureGroup();
 layerControl.addOverlay(awsLayer, "Wetterstationen Tirol");
-awsLayer.addTo(map);
+// awsLayer.addTo(map);
+let snowLayer = L.featureGroup();
+layerControl.addOverlay(snowLayer, "Schneehöhen");
+snowLayer.addTo(map);
+
 
 fetch(awsUrl)
     .then(response => response.json())
@@ -49,6 +53,18 @@ fetch(awsUrl)
             <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
             `);
             marker.addTo(awsLayer);
+            if (station.properties.HS) {
+                let snowIcon = L.divIcon({
+                    html: `<div class="snow-label">${station.properties.HS}</div>`
+                })
+                let snowMarker = L.marker([
+                    station.geometry.coordinates[1],
+                    station.geometry.coordinates[0]
+                ], {
+                    icon: snowIcon
+                });
+                snowMarker.addTo(snowLayer);
+            }
         }
         // set map view to all stations
         map.fitBounds(awsLayer.getBounds());
