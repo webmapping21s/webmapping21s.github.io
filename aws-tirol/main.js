8013948fcb186b878f323a4e879558d5311e7cff
Bernd Oeggl <bernd.oeggl@uibk.ac.at>
2021-04-15 16:16:39 +0200

    add layergroup with overlay and ortho

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -11,5 +11,10 @@ let map = L.map("map", {
 
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
-    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto')
+    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
+    "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
+    "BasemapAT.overlay+ortho": L.layerGroup([
+        L.tileLayer.provider('BasemapAT.orthofoto'),
+        L.tileLayer.provider('BasemapAT.overlay')
+    ])
 }).addTo(map);
