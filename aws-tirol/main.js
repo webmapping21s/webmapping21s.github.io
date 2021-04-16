88ba4d11f5507bb162e40deefc2b8249b8b0efbc
Klaus Förster <openwebcc@github.com>
2021-04-16 20:31:41 +0200

    fix typo

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -51,7 +51,7 @@ fetch(awsUrl)
               <li>Temperatur: ${station.properties.LT} C</li>
               <li>Schneehöhe: ${station.properties.HS || '?'} cm</li>
               <li>Windgeschwindigkeit: ${station.properties.WG || '?'} km/h</li>
-              <li>Windgeschwindrichtung: ${station.properties.WR || '?'}</li>
+              <li>Windrichtung: ${station.properties.WR || '?'}</li>
             </ul>
             <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
             `);
