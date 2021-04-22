a74e05665959772f20933b5df3cd77eb9cd183ef
Klaus Förster <klaus.foerster@uibk.ac.at>
2021-04-22 17:57:39 +0200

    Farbpaletten als Option übergeben und getColor Funktion Teil 1

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -40,7 +40,12 @@ L.control.scale({
     imperial: false
 }).addTo(map);
 
+let getColor = (value, colorRamp) => {
+    console.log("Wert:", value, "Palette:", colorRamp);
+};
+
 let newLabel = (coords, options) => {
+    let color = getColor(options.value, options.colors)
     let label = L.divIcon({
         html: `<div>${options.value}</div>`,
         className: "text-label"
@@ -78,19 +83,22 @@ fetch(awsUrl)
             marker.addTo(overlays.stations);
             if (typeof station.properties.HS == "number") {
                 let marker = newLabel(station.geometry.coordinates, {
-                    value: station.properties.HS
+                    value: station.properties.HS,
+                    colors: COLORS.snowheight
                 });
                 marker.addTo(overlays.snowheight);
             }
             if (typeof station.properties.WG == "number") {
                 let marker = newLabel(station.geometry.coordinates, {
-                    value: station.properties.WG
+                    value: station.properties.WG,
+                    colors: COLORS.windspeed
                 });
                 marker.addTo(overlays.windspeed);
             }
             if (typeof station.properties.LT == "number") {
                 let marker = newLabel(station.geometry.coordinates, {
-                    value: station.properties.LT
+                    value: station.properties.LT,
+                    colors: COLORS.temperature
                 });
                 marker.addTo(overlays.temperature);
             }
