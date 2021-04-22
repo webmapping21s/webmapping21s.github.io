1d1e80ca39dd42179add50feeb552883670eaf6e
Klaus Förster <klaus.foerster@uibk.ac.at>
2021-04-22 18:23:46 +0200

    Tooltip für Stationsname und Höhe

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -58,7 +58,8 @@ let newLabel = (coords, options) => {
         className: "text-label"
     })
     let marker = L.marker([coords[1], coords[0]], {
-        icon: label
+        icon: label,
+        title: `${options.station} (${coords[2]}m)`
     });
     return marker;
 };
@@ -91,21 +92,24 @@ fetch(awsUrl)
             if (typeof station.properties.HS == "number") {
                 let marker = newLabel(station.geometry.coordinates, {
                     value: station.properties.HS.toFixed(0),
-                    colors: COLORS.snowheight
+                    colors: COLORS.snowheight,
+                    station: station.properties.name
                 });
                 marker.addTo(overlays.snowheight);
             }
             if (typeof station.properties.WG == "number") {
                 let marker = newLabel(station.geometry.coordinates, {
                     value: station.properties.WG.toFixed(0),
-                    colors: COLORS.windspeed
+                    colors: COLORS.windspeed,
+                    station: station.properties.name
                 });
                 marker.addTo(overlays.windspeed);
             }
             if (typeof station.properties.LT == "number") {
                 let marker = newLabel(station.geometry.coordinates, {
                     value: station.properties.LT.toFixed(1),
-                    colors: COLORS.temperature
+                    colors: COLORS.temperature,
+                    station: station.properties.name
                 });
                 marker.addTo(overlays.temperature);
             }
