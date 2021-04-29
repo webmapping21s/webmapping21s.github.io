098e34fcdb6cf565f9b1f3dac1517a08c156d92e
Bernd Oeggl <bernd.oeggl@uibk.ac.at>
2021-04-29 17:29:13 +0200

    add styling for line elements in geojson

--- wien/main.js
+++ wien/main.js
@@ -67,14 +67,37 @@ let drawBusStop = (geojsonData) => {
     }).addTo(overlays.busStops);
 }
 
+let drawBusLines = (geojsonData) => {
+    console.log('Bus Lines: ', geojsonData);
+    L.geoJson(geojsonData, {
+        style: (feature) => {
+            let col = "red";
+            if (feature.properties.LINE_NAME == 'Blue Line') {
+                col = "blue";
+            }
+            return {
+                color: col
+            }
+        },
+        onEachFeature: (feature, layer) => {
+            layer.bindPopup(`<strong>${feature.properties.LINE_NAME}</strong>
+            <hr>
+            von ${feature.properties.FROM_NAME}<br>
+            nach ${feature.properties.TO_NAME}`)
+        }
+    }).addTo(overlays.busLines);
+}
+
 for (let config of OGDWIEN) {
-    console.log("Config: ", config.data);
+    // console.log("Config: ", config.data);
     fetch(config.data)
         .then(response => response.json())
         .then(geojsonData => {
-            console.log("Data: ", geojsonData);
+            // console.log("Data: ", geojsonData);
             if (config.title == "Haltestellen Vienna Sightseeing") {
                 drawBusStop(geojsonData);
+            } else if (config.title == "Liniennetz Vienna Sightseeing") {
+                drawBusLines(geojsonData);
             }
         })
 }
