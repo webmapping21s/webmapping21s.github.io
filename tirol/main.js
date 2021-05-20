b9fac3fa7745e59be31788a6d8991a164f5d08d6
Bernd Oeggl <bernd.oeggl@uibk.ac.at>
2021-05-20 16:01:38 +0200

    GPX track loaded with icons

--- tirol/main.js
+++ tirol/main.js
@@ -44,6 +44,14 @@ overlays.tracks.addTo(map);
 
 const drawTrack = (nr) => {
     console.log('Track: ', nr);
+    let gpxTrack = new L.GPX(`tracks/${nr}.gpx`, {
+        async: true,
+        marker_options: {
+            startIconUrl: `icons/number_${nr}.png`,
+            endIconUrl: 'icons/finish.png',
+            shadowUrl: null,
+        }
+    }).addTo(overlays.tracks);
 };
 
 const selectedTrack = 7;
