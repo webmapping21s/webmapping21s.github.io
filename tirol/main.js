49d4ff01344e796edd034a346eaaf292a652d99f
Bernd Oeggl <bernd.oeggl@uibk.ac.at>
2021-05-20 16:07:34 +0200

    gpx loaded event with zoom to bounds

--- tirol/main.js
+++ tirol/main.js
@@ -43,15 +43,23 @@ let layerControl = L.control.layers({
 overlays.tracks.addTo(map);
 
 const drawTrack = (nr) => {
-    console.log('Track: ', nr);
+    // console.log('Track: ', nr);
     let gpxTrack = new L.GPX(`tracks/${nr}.gpx`, {
         async: true,
         marker_options: {
             startIconUrl: `icons/number_${nr}.png`,
             endIconUrl: 'icons/finish.png',
             shadowUrl: null,
-        }
+        },
+        polyline_options: {
+            color: 'black',
+            dashArray: [2, 5],
+        },
     }).addTo(overlays.tracks);
+    gpxTrack.on("loaded", () => {
+        console.log('loaded gpx');
+        map.fitBounds(gpxTrack.getBounds());
+    })
 };
 
 const selectedTrack = 7;
