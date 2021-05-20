77798df98e683a3c3ab8fdbee3a67e1460bfdab3
Bernd Oeggl <bernd.oeggl@uibk.ac.at>
2021-05-20 17:40:54 +0200

    remove debug logging

--- tirol/main.js
+++ tirol/main.js
@@ -65,9 +65,9 @@ const drawTrack = (nr) => {
         },
     }).addTo(overlays.tracks);
     gpxTrack.on("loaded", () => {
-        console.log('loaded gpx');
+        // console.log('loaded gpx');
         map.fitBounds(gpxTrack.getBounds());
-        console.log('Track name: ', gpxTrack.get_distance());
+        // console.log('Track name: ', gpxTrack.get_distance());
         gpxTrack.bindPopup(`
         <h3>${gpxTrack.get_name()}</h3>
         <ul>
