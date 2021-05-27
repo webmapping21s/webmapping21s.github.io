a385fbd0c143ac6ee0b04043a9788354afef9981
Klaus Förster <klaus.foerster@uibk.ac.at>
2021-05-27 16:48:23 +0200

    console.log aufrufe abdrehen

--- tirol/main.js
+++ tirol/main.js
@@ -55,9 +55,9 @@ const elevationControl = L.control.elevation({
 // Wikipedia Artikel Zeichnen
 let articleDrawn = {};
 const drawWikipedia = (bounds) => {
-    console.log(bounds);
+    //console.log(bounds);
     let url = `https://secure.geonames.org/wikipediaBoundingBoxJSON?north=${bounds.getNorth()}&south=${bounds.getSouth()}&east=${bounds.getEast()}&west=${bounds.getWest()}&username=webmapping&lang=de&maxRows=30`;
-    console.log(url);
+    //console.log(url);
 
     let icons = {
         adm1st: "wikipedia_administration.png",
@@ -78,14 +78,14 @@ const drawWikipedia = (bounds) => {
     fetch(url).then(
         response => response.json()
     ).then(jsonData => {
-        console.log(jsonData);
+        //console.log(jsonData);
 
         // Artikel Marker erzeugen
         for (let article of jsonData.geonames) {
             // habe ich den Artikel schon gezeichnet?
             if (articleDrawn[article.wikipediaUrl]) {
                 // Ja, nicht noch einal zeichnen
-                console.log("schon gesehen", article.wikipediaUrl);
+                //console.log("schon gesehen", article.wikipediaUrl);
                 continue;
             } else {
                 articleDrawn[article.wikipediaUrl] = true;
