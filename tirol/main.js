9d8dd36a7e1aa0dedaaed91c293ce166b347b07f
Klaus Förster <klaus.foerster@uibk.ac.at>
2021-05-27 16:16:20 +0200

    Artikel Popup fertig machen

--- tirol/main.js
+++ tirol/main.js
@@ -69,11 +69,20 @@ const drawWikipedia = (bounds) => {
             let mrk = L.marker([article.lat, article.lng]);
             mrk.addTo(overlays.wikipedia);
 
-            // Popup erzeugen
+            // optionales Bild definieren
             let img = "";
             if (article.thumbnailImg) {
                 img = `<img src="${article.thumbnailImg}" alt="thumbnail">`;
             }
+
+            // Popup definieren
+            mrk.bindPopup(`
+                <small>${article.feature}</small>
+                <h3>${article.title} (${article.elevation}m)</h3>
+                ${img}
+                <p>${article.summary}</p>
+                <a target="Wikipedia" href="https://${article.wikipediaUrl}">Wikipedia-Artikel</a>
+            `)
         }
     });
 };
